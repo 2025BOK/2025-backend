ada411f644fc0de5a0293bd0df8693285a09d17a
unknown <duekdud01@naver.com>
2025-04-21 18:01:23 +0900

    [feat] StoreService 업로드

--- backend-project/src/main/java/kr/co/bapsang/backend_project/service/StoreService.java
+++ backend-project/src/main/java/kr/co/bapsang/backend_project/service/StoreService.java
@@ -1,4 +1,26 @@
 package kr.co.bapsang.backend_project.service;
 
+import kr.co.bapsang.backend_project.dto.StoreDto;
+import kr.co.bapsang.backend_project.entity.StoreEntity;
+import kr.co.bapsang.backend_project.repository.StoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class StoreService {
+    private final StoreRepository storeRepository;
+
+    public ResponseEntity createStore(StoreDto dto) {
+        StoreEntity newStore = new StoreEntity();
+        newStore.setSTORE_NO(dto.getSTORE_NO());
+        newStore.setSTORE_NM(dto.getSTORE_NM());
+        newStore.setSTORE_LOC(dto.getSTORE_LOC());
+        storeRepository.save(newStore);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
 }
