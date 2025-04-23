96193da51e16d343b72ac4a8159fba07e1be7ec2
unknown <duekdud01@naver.com>
2025-04-23 10:08:17 +0900

    [feat] 가게 등록, 조회, 수정, 삭제 기능 구현 완료

--- backend-project/src/main/java/kr/co/bapsang/backend_project/controller/StoreController.java
+++ backend-project/src/main/java/kr/co/bapsang/backend_project/controller/StoreController.java
@@ -1,4 +1,53 @@
 package kr.co.bapsang.backend_project.controller;
 
+import kr.co.bapsang.backend_project.dto.StoreDto;
+import kr.co.bapsang.backend_project.entity.StoreEntity;
+import kr.co.bapsang.backend_project.repository.StoreRepository;
+import kr.co.bapsang.backend_project.service.StoreService;
+import kr.co.bapsang.backend_project.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("store")
 public class StoreController {
+
+    private final StoreService storeService;
+    private final StoreRepository storeRepository;
+
+    @PostMapping("register")
+    @CrossOrigin
+    public ResponseEntity createStore(@RequestBody StoreDto dto) {
+        return storeService.createStore(dto);
+    }
+
+    @PutMapping("{storeNo}")
+    @CrossOrigin
+    public ResponseEntity updateStore(@PathVariable Long storeNo, @RequestBody StoreDto dto) {
+        return storeService.updateStore(storeNo, dto);
+    }
+
+    @DeleteMapping("{storeNo}")
+    @CrossOrigin
+    public ResponseEntity deleteStore(@PathVariable Long storeNo) {
+        return storeService.deleteStore(storeNo);
+    }
+
+    @GetMapping
+    @CrossOrigin
+    public ResponseEntity<List> getStoreList(@RequestBody String storeNm) {
+        return storeService.readStoreList(storeNm);
+    }
+
+    @GetMapping("{storeNo}")
+    @CrossOrigin
+    public ResponseEntity<StoreEntity> getStore(@PathVariable Long storeNo) {
+        return storeService.readStore(storeNo);
+    }
+
 }
