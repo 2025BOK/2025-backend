5c43fd36aa56ec3a71e755ca5a36630ebbc0e257
karamjoen <duddj1203@naver.com>
2025-05-07 23:13:09 +0900

    feat: update

--- backend-project/src/main/java/kr/co/bapsang/backend_project/controller/ReviewController.java
+++ backend-project/src/main/java/kr/co/bapsang/backend_project/controller/ReviewController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+//
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
